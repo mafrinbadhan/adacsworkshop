b0d48affffba3cb67e96cc264520531ac3c1f8a0
Mahmuda Afrin Badhan <afrin20m@terpmail.umd.edu>
2023-03-20 14:18:16 +1100

    Uppercased the constants RA, DEC etc improved import statements after moving them to the top, added doc strings, closing file at the end

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,26 +1,28 @@
+"""
 # Determine Andromeda location in ra/dec degrees
+"""
+# convert to decimal degrees
+from math import cos, pi
+from random import uniform
 
 # from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+RA = '00:42:44.3'
+DEC = '41:16:09'
 
-# convert to decimal degrees
-from math import *
 
-d, m, s = dec.split(':')
+d, m, s = DEC.split(':')
 dec = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = ra.split(':')
+h, m, s = RA.split(':')
 ra = 15*(int(h)+int(m)/60+float(s)/3600)
 ra = ra/cos(dec*pi/180)
 
-nsrc = 1000000
+NSRC = 1000000
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
 ras = []
 decs = []
-for i in range(nsrc):
+for i in range(NSRC):
     ras.append(ra + uniform(-1,1))
     decs.append(dec + uniform(-1,1))
 
@@ -28,5 +30,6 @@ for i in range(nsrc):
 # now write these to a csv file for use by my other program
 f = open('catalog.csv','w')
 print ("id,ra,dec", file=f)
-for i in range(nsrc):
+for i in range(NSRC):
     print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+f.close()
