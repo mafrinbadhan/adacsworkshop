1d8f3e588b2cac11c63266a74a47cfcc2608ee28
Mahmuda Afrin Badhan <afrin20m@terpmail.umd.edu>
2023-03-20 15:26:38 +1100

    changed code to function based coding and included main and fixed a lot of indentation consistencies

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,36 +1,61 @@
 """
-# Determine Andromeda location in ra/dec degrees
+
 """
 # convert to decimal degrees
 from math import cos, pi
 from random import uniform
 
+NSRC = 1000000
 # from wikipedia
 RA = '00:42:44.3'
 DEC = '41:16:09'
 
+def get_radec(RA, DEC):
+    """
+    # Determine Andromeda location in ra/dec degrees
+    """
 
-d, m, s = DEC.split(':')
-dec = int(d)+int(m)/60+float(s)/3600
+    d, m, s = DEC.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = RA.split(':')
-ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/cos(dec*pi/180)
+    h, m, s = RA.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
 
-NSRC = 1000000
+    return(ra, dec)
 
 # make 1000 stars within 1 degree of Andromeda
-ras = []
-decs = []
-for i in range(NSRC):
-    ras.append(ra + uniform(-1,1))
-    decs.append(dec + uniform(-1,1))
+def make_stars(ra, dec, NSRC):
+    ras = []
+    decs = []
+    for i in range(NSRC):
+        ras.append(ra + uniform(-1,1))
+        decs.append(dec + uniform(-1,1))
 
+    return(ras, decs)
+
+
+def main():
+    ra, dec = get_radec(RA, DEC)
+    ras, decs = make_stars(ra, dec, NSRC)
+  
+    with open('catalog.csv','w', encoding='utf-8') as f:
+        print ("id,ra,dec", file=f)
+        for i in range(NSRC):
+		#print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+
+if __name__ == '__main__':
+    main()
 
 # now write these to a csv file for use by my other program
-with open('catalog.csv','w', encoding='utf-8') as f:
-    print ("id,ra,dec", file=f)
-    for i in range(NSRC):
-		#print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
-        print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+
 #f.close()
+
+
+#import mymodule.sky_sim as ss
+
+#ss.get_radec()
+#ss.make_stars(10, -53, 10)
+#help(ss.get_radec)
+#ss.make_stars(*a, 10)
